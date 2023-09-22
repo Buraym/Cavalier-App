6471ceb095a480f85e5cf56f023de1df7d41ecde
Buraym <70729051+Buraym@users.noreply.github.com>
2023-09-22 20:10:00 -0300

    [ Feat ]: Added locale and localization fo english and spanish in login page

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router';
+import { createI18n } from "vue-i18n";
+import es from "./locales/es.json";
+import pt from "./locales/pt.json";
+import en from "./locales/en.json";
 import { init_db } from './utils/database';
 import { createPinia } from "pinia"
+import App from './App.vue'
+import router from './router';
+
+const i18n = createI18n({
+    locale: "pt",
+    fallbackLocale: "pt",
+    messages: { pt, en, es },
+  });
+
 const pinia = createPinia()
 await init_db();
-createApp(App).use(router).use(pinia).mount('#app')
+
 
